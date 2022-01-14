ae01899878bce812599282c9ee0b73ecc751b6c7
neutralboy <leopoorna99@gmail.com>
2022-01-15 01:01:12 +0530

    added basic files

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+// tailwind.config.cjs
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+      fontFamily: {
+          body: ['Work Sans', 'sans-serif'],
+          display: ['Merriweather', 'serif']
+      },
+      extend: {
+          colors: {
+              theme: "#b7d0f7",
+              "theme-true": "#00c2cb",
+              contrast: "#fbbc04",
+              offWhite: "#f6fbfd"
+          }
+      }
+  }
+};
